e0aaa2beac59d39792ce967f923a03427198f50a
umer majeed <umermjd11@gmail.com>
2024-11-12 17:18:15 +0000

    porfolio example ready

--- w2v_utils.py
+++ w2v_utils.py
@@ -1,7 +1,8 @@
 from keras.models import Model
-from keras.layers import Input, Dense, Reshape, merge
-from keras.layers.embeddings import Embedding
-from keras.preprocessing.sequence import skipgrams
+from keras.layers import Input, Dense, Reshape
+from keras.layers import Concatenate, Add
+from keras.layers import Embedding
+from tensorflow.keras.preprocessing.sequence import skipgrams
 from keras.preprocessing import sequence
 
 import urllib.request
